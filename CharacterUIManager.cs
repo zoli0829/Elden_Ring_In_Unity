475e48cc531fbcdf7117cc42ea862d50cfde4656
ZoltanV <zoli0829@gmail.com>
2024-03-10 13:11:26 +0000

    Relocated some files to Scripts

---
+++ CharacterUIManager.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace ZV
+{
+    public class CharacterUIManager : MonoBehaviour
+    {
+        [Header("UI")]
+        public bool hasFloatingHPBar = true;
+        public UI_Character_HP_Bar characterHPBar;
+
+        public void OnHPChanged(int oldValue, int newValue)
+        {
+            characterHPBar.oldHealthValue = oldValue;
+            characterHPBar.SetStat(newValue);
+        }
+    }
+}
